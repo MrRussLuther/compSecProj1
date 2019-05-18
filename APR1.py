31e79a6adfd2566d3dc85369e124a51bcf5cbe36
MrRussLuther <rluther@broadinstitute.org>
2019-05-18 03:56:54 -0400

    verify step for APR1

--- APR1.py
+++ APR1.py
@@ -13,4 +13,10 @@ apr3 = apr.hash(myPass3, salt=salt3)
 
 print("APR Hash of {}: {}".format(myPass1, apr1))
 print("APR Hash of {}: {}".format(myPass2, apr2))
-print("APR Hash of {}: {}".format(myPass3, apr3))
+print("APR Hash of {}: {}\n".format(myPass3, apr3))
+
+print("APR Verification of Hash {}: {}".format(myPass1, apr.verify(myPass1, apr1)))
+print("APR Verification of Hash {}: {}".format(myPass2, apr.verify(myPass2, apr2)))
+print("APR Verification of Hash {}: {}\n".format(myPass3, apr.verify(myPass3, apr3)))
+
+print("Dummy test of Hash {}: {}".format(myPass3, apr.verify("passward", apr3)))
